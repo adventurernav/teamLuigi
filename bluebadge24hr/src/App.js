c391886b8c901780c28caafb0d8fd5ed8357a83d
Nav Loveday <twelvetreeorchard@gmail.com>
2020-09-08 15:00:24 -0400

    location update

--- bluebadge24hr/src/App.js
+++ bluebadge24hr/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import getLocation from './components/site/location'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <getLocation />
     </div>
   );
 }
