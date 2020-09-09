7ee4c9a6a7805316a0cee90f86b954bc2d4a084a
Devin Owen <devinjamesowen@gmail.com>
2020-09-08 20:42:15 -0400

    another commit

--- bluebadge24hr/src/components/apps/Weather.js
+++ bluebadge24hr/src/components/apps/Weather.js
@@ -1,16 +1,20 @@
 import React from 'react';
 
-const baseURL = api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={'4fff3873910c61a841f80b62b0dfccf8'};
 const key = '4fff3873910c61a841f80b62b0dfccf8';
+const baseURL = `api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${key}`;
 
-const weather = () => {
+const weather = (props) => {
 
     fetch(baseURL)
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => console.log(data))
     .catch(err => console.log(err));
+
+    temperature = '';
+    let celcius = props.temperature;
+    let farenheight=(props.temperature *9)/5
 
 }
 
 
-
+export default weather; 
--- bluebadge24hr/src/components/site/sidebar.js
+++ bluebadge24hr/src/components/site/sidebar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Home from './home';
 // import NasaApp from '../apps/Nasa'
-// import Weather from '../apps/Weather'
+import Weather from '../apps/Weather'
 // import Zomato from '../apps/Zomato'
 import {
     Route,
@@ -18,7 +18,7 @@ const sidebar =(props) => {
 
                 <li><Link to = '/'>Home</Link> </li>
                 {/* <li><Link to = '/nasa'>NASA</Link> </li> */}
-                {/* <li><Link to = '/weather'>Weather</Link> </li> */}
+                <li><Link to = '/weather'>Weather</Link> </li>
                 {/* <li><Link to = '/zomato'>Zomato</Link> </li> */}
 
 
@@ -29,7 +29,7 @@ const sidebar =(props) => {
                 <Switch>
                     <Route exact path = '/home'><Home /></Route>
                     {/* <Route exact path = '/nasa'><NasaApp /></Route> */}
-                    {/* <Route exact path = '/weather'><Weather /></Route> */}
+                    <Route exact path = '/weather'><Weather /></Route>
                     {/* <Route exact path = '/zomato'><Zomato /></Route> */}
                 </Switch>
 
